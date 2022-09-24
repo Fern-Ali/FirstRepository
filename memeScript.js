ecab7ae6a7505f0698a2ca473292c2c3255daedc
alicia fernandez <fernandez.m.alicia2020@gmail.com>
2022-09-24 15:15:29 -0400

    removed listener that was constantly calling setBackground and updated media queries for sub 700px bpd

--- memeScript.js
+++ memeScript.js
@@ -55,53 +55,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-        /*this was an attempt to make the meme dissapear completely from localStorage when you hit the X button. Not functional*/
-
-        //document.addEventListener("click", function (event) {
-
-        //    /*console.log(event.target.innerText);*/
-        //    if (event.target.innerText === "X") {
-        //        //let buttonHolder = event.target.parentNode;
-        //        //buttonHolder.parentNode.remove();
-                
-        //        savedMemes.pop();
-        //    }
-
-        //});
-
-
     }
 
     const xButton = JSON.parse(localStorage.getItem("xButton"));
 
 
+    let x = window.matchMedia("(max-width: 700px)")
+    let y = window.matchMedia("(max-width: 1900px")
+    setBackground(x) // Call listener function at run time
+    //x.addListener(setBackground) // Attach listener function on state changes
+    //y.addListener(setBackground)  //I removed the listener, it kept on constantly calling the function
 
-    //function test() {
-    //    let backgroundArr = [
-    //        `"url('https://images.unsplash.com/photo-1662668862763-dc613ee191ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')"`,
-    //        `"url('https://wallpaperaccess.com/full/5620839.jpg')"`,
-    //        `"url('https://newevolutiondesigns.com/images/freebies/cat-wallpaper-3.jpg')"`,
-    //        `"url('https://i.imgur.com/tYcH3vL.jpeg')",`
-    //        `"url('https://i.pinimg.com/originals/09/6a/35/096a35453660aa9b83ba4ab6d9182d61.jpg')"`,
-    //        `"url('https://www.pixelstalk.net/wp-content/uploads/2016/07/Forests-bridges-natural-scenery-hd-wallpaper-backgrounds-1920x1080.jpg')"`,
-    //        `"url('https://wallpaperaccess.com/full/1132043.jpg')"`,
-    //        `"url('https://images.unsplash.com/photo-1662574429291-5d9c9b2ff6c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80')"`,
-    //        `"url('https://images.wallpapersden.com/image/download/4k-natural-gradient_a2xsZmiUmZqaraWkpJRmbmdlrWZlbWU.jpg')"`,
-    //        `"url('https://images4.alphacoders.com/663/663219.jpg')"`
-    //    ]
-    //    for (let i = 0; i < backgroundArr.length; i++) {
-
-    //        window.setInterval(function (e) {
-
-    //            document.body.style.backgroundImage = backgroundArr[i];
-    //        }, 30000);
-
-    //    }
-    //}
-
-    //randomizer for background Image
-
-    /*test();*/
 
     function setBackground(x) {
         if (x.matches) { // If media query matches
@@ -146,9 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    let x = window.matchMedia("(max-width: 700px)")
-    setBackground(x) // Call listener function at run time
-    x.addListener(setBackground) // Attach listener function on state changes
+    
 
 
     
@@ -158,10 +120,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let formDiv = document.getElementById("form-div");
 
 
-    /*moved this styling to the style sheet to make it easier to resize for different viewports*/
-
-
-    /*document.body.style.backgroundRepeat = "no-repeat";*/
+/*moved this styling to the style sheet to make it easier to resize for different viewports*/
+/*document.body.style.backgroundRepeat = "no-repeat";*/
     
 
    
@@ -193,10 +153,7 @@ document.addEventListener("DOMContentLoaded", function () {
         savedMemes.push({ img: `url('${document.getElementById("img").value}')`, topTextLine: document.getElementById("top-line").value, bottomTextLine: document.getElementById("bottom-line").value })
 
         localStorage.setItem("memes", JSON.stringify(savedMemes));
-        //savedMeme.push({ task: newTodo.innerText, isCompleted: false });
-
-        //newTodo.appendChild(removeButton);
-        //localStorage.setItem("todos", JSON.stringify(savedTodos));
+        
     });
 
 
