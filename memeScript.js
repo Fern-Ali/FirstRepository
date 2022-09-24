6b8154fd6bef6b3a0d5e41d06a894bd5bbdde120
alicia fernandez <fernandez.m.alicia2020@gmail.com>
2022-09-24 02:06:04 -0400

    changed 700px breakpoint to resort to single image background no rotating img

--- memeScript.js
+++ memeScript.js
@@ -103,43 +103,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /*test();*/
 
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            document.body.style.backgroundImage = "url('https://wallpaper.dog/large/20492556.jpg')";
+        
+        } else {
+            randomize();
+            setInterval(randomize, 7000);
+            function randomize() {
+                let randomInt = Math.floor(Math.random() * 10);
+                if (randomInt === 1) {
+                    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1662668862763-dc613ee191ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')";
+                } else if (randomInt === 2) {
+                    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/5620839.jpg')";
+                } else if (randomInt === 3) {
+                    document.body.style.backgroundImage = "url('https://newevolutiondesigns.com/images/freebies/cat-wallpaper-3.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 4) {
+                    document.body.style.backgroundImage = "url('https://i.imgur.com/tYcH3vL.jpeg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 5) {
+                    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/09/6a/35/096a35453660aa9b83ba4ab6d9182d61.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 6) {
+                    document.body.style.backgroundImage = "url('https://www.pixelstalk.net/wp-content/uploads/2016/07/Forests-bridges-natural-scenery-hd-wallpaper-backgrounds-1920x1080.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 7) {
+                    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 8) {
+                    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 9) {
+                    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                } else if (randomInt === 0) {
+                    document.body.style.backgroundImage = "url('https://images4.alphacoders.com/663/663219.jpg')";
+                    /*document.body.style.backgroundSize = "cover";*/
+                }
 
-    randomize();
-    setInterval(randomize, 7000);
-
-    function randomize() {
-        let randomInt = Math.floor(Math.random() * 10);
-        if (randomInt === 1) {
-            document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1662668862763-dc613ee191ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')";
-        } else if (randomInt === 2) {
-            document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/5620839.jpg')";
-        } else if (randomInt === 3) {
-            document.body.style.backgroundImage = "url('https://newevolutiondesigns.com/images/freebies/cat-wallpaper-3.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 4) {
-            document.body.style.backgroundImage = "url('https://i.imgur.com/tYcH3vL.jpeg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 5) {
-            document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/09/6a/35/096a35453660aa9b83ba4ab6d9182d61.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 6) {
-            document.body.style.backgroundImage = "url('https://www.pixelstalk.net/wp-content/uploads/2016/07/Forests-bridges-natural-scenery-hd-wallpaper-backgrounds-1920x1080.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 7) {
-            document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 8) {
-            document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 9) {
-            document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1132043.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
-        } else if (randomInt === 0) {
-            document.body.style.backgroundImage = "url('https://images4.alphacoders.com/663/663219.jpg')";
-            /*document.body.style.backgroundSize = "cover";*/
+            }
         }
-
     }
+
+    let x = window.matchMedia("(max-width: 700px)")
+    myFunction(x) // Call listener function at run time
+    x.addListener(myFunction) // Attach listener function on state changes
+
+
+    
+
+
 
     let formDiv = document.getElementById("form-div");
 
